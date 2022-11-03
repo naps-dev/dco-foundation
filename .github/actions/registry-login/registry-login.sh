9a4eb141b04cd2e0c4945b927f558997ae0afe80
Mike Vanhemert <mike.vanhemert@defenseunicorns.com>
2022-11-03 17:10:10 -0600

    Debug

--- .github/actions/registry-login/registry-login.sh
+++ .github/actions/registry-login/registry-login.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+echo $1
+
 if [ -n "$1" ]; then
   REGISTRY1_USERNAME=$1
 else
   echo "error: registry username must be defined"
-  return 1
+  exit 1
 fi
 
 if [ -n "$2" ]; then
   REGISTRY1_PASSWORD=$2
 else
   echo "error: registry password must be defined"
-  return 1
+  exit 1
 fi
 
 zarf tools registry login -u $REGISTRY1_USERNAME -p $REGISTRY1_PASSWORD
