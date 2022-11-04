0921ba4410f8bf34037a9e445483c2e5e9ff7b82
Blake Burkhart <bburky@bburky.com>
2022-11-04 11:05:07 -0500

    quote bash variables

--- .github/actions/package-create/package-create.sh
+++ .github/actions/package-create/package-create.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ -n "$1" ]; then
-  BRANCH=$1
+  BRANCH="$1"
 else
   BRANCH="main"
 fi
 
-zarf package create --set BRANCH=$BRANCH --confirm --skip-sbom
+zarf package create --set BRANCH="$BRANCH" --confirm --skip-sbom
--- .github/actions/registry-login/registry-login.sh
+++ .github/actions/registry-login/registry-login.sh
@@ -3,24 +3,24 @@
 echo $1
 
 if [ -n "$1" ]; then
-  REGISTRY_USERNAME=$1
+  REGISTRY_USERNAME="$1"
 else
   echo "error: registry username must be defined"
   exit 1
 fi
 
 if [ -n "$2" ]; then
-  REGISTRY_PASSWORD=$2
+  REGISTRY_PASSWORD="$2"
 else
   echo "error: registry password must be defined"
   exit 1
 fi
 
 if [ -n "$3" ]; then
-  REGISTRY_URL=$3
+  REGISTRY_URL="$3"
 else
   echo "error: registry must be defined"
   exit 1
 fi
 
-zarf tools registry login -u $REGISTRY_USERNAME -p $REGISTRY_PASSWORD $REGISTRY_URL
+zarf tools registry login -u "$REGISTRY_USERNAME" -p "$REGISTRY_PASSWORD" "$REGISTRY_URL"
