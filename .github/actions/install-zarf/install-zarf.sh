4c400f0055670f635d13e52bd58decdda5d58e43
Blake Burkhart <bburky@bburky.com>
2022-11-08 14:40:38 -0600

    Revert "revert install-zarf"
    This reverts commit b746145eb6e86cd811eea3bc501ed2243e09f981.

--- .github/actions/install-zarf/install-zarf.sh
+++ .github/actions/install-zarf/install-zarf.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget https://github.com/defenseunicorns/zarf/releases/download/v0.22.2/zarf_v0.22.2_Linux_amd64
+curl -s -O https://github.com/defenseunicorns/zarf/releases/download/v0.22.2/zarf_v0.22.2_Linux_amd64
 sudo install -g root -o root -m 0777 zarf_v0.22.2_Linux_amd64 /usr/local/bin/zarf
 zarf version
